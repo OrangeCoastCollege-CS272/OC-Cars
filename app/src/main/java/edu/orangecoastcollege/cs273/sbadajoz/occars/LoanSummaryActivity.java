06132eb84a8cd029f093d7403a055ccf6a75abff
Badajoz <sbadajoz@student.cccd.edu>
2017-09-14 16:51:34 -0700

    Move method to correct class, create reportSummary method

--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/occars/LoanSummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/occars/LoanSummaryActivity.java
@@ -1,36 +1,14 @@
 package edu.orangecoastcollege.cs273.sbadajoz.occars;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
-import android.widget.RadioButton;
 
-public class LoanSummaryActivity extends AppCompatActivity {
-    private EditText mCarPriceEditText;
-    private EditText mDownPaymentEditText;
-    private RadioButton mThreeYearsRadioButton;
-    private RadioButton mFourYearsRadioButton;
-    private RadioButton mFiveYearsRadioButton;
-
-    private CarLoan mCarLoan = new CarLoan();
-
+public class LoanSummaryActivity extends AppCompatActivity {private EditText mCarPriceEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_summary);
-
-        mCarPriceEditText = (EditText) findViewById(R.id.carPriceEditText);
-        mDownPaymentEditText = (EditText) findViewById(R.id.downPaymentEditText);
-        mThreeYearsRadioButton = (RadioButton) findViewById(R.id.threeYearsRadioButton);
-        mFourYearsRadioButton = (RadioButton) findViewById(R.id.fourYearsRadioButton);
-        mFiveYearsRadioButton = (RadioButton) findViewById(R.id.fiveYearsRadioButton);
-    }
-
-    private void collectCarLoanData() {
-        mCarLoan.setPrice(Double.parseDouble(mCarPriceEditText.getText().toString()));
-        mCarLoan.setDownPayment(Double.parseDouble(mDownPaymentEditText.getText().toString()));
-        if(mThreeYearsRadioButton.isChecked()) mCarLoan.setTerm(3);
-        else if(mFourYearsRadioButton.isChecked()) mCarLoan.setTerm(4);
-        else mCarLoan.setTerm(5);
     }
 }
+
--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/occars/PurchaseActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/occars/PurchaseActivity.java
@@ -1,18 +1,52 @@
 package edu.orangecoastcollege.cs273.sbadajoz.occars;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class PurchaseActivity extends AppCompatActivity {
+    private EditText mCarPriceEditText;
+    private EditText mDownPaymentEditText;
+    private RadioButton mThreeYearsRadioButton;
+    private RadioButton mFourYearsRadioButton;
+    private RadioButton mFiveYearsRadioButton;
+
+    private CarLoan mCarLoan = new CarLoan();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase);
+
+        mCarPriceEditText = (EditText) findViewById(R.id.carPriceEditText);
+        mDownPaymentEditText = (EditText) findViewById(R.id.downPaymentEditText);
+        mThreeYearsRadioButton = (RadioButton) findViewById(R.id.threeYearsRadioButton);
+        mFourYearsRadioButton = (RadioButton) findViewById(R.id.fourYearsRadioButton);
+        mFiveYearsRadioButton = (RadioButton) findViewById(R.id.fiveYearsRadioButton);
     }
 
-    protected void reportSummary(View view) {
-        String report = "Monthy Payment = ";
+    private void collectCarLoanData() {
+        mCarLoan.setPrice(Double.parseDouble(mCarPriceEditText.getText().toString()));
+        mCarLoan.setDownPayment(Double.parseDouble(mDownPaymentEditText.getText().toString()));
+        if(mThreeYearsRadioButton.isChecked()) mCarLoan.setTerm(3);
+        else if(mFourYearsRadioButton.isChecked()) mCarLoan.setTerm(4);
+        else mCarLoan.setTerm(5);
     }
+
+    protected void reportSummary(View v) {
+        collectCarLoanData();
+
+        StringBuilder report = new StringBuilder();
+
+        report.append("Monthly Payment: $").append(mCarLoan.monthlyPayment()).append("\n");
+
+
+        Intent launchLoanSummary = new Intent(this, LoanSummaryActivity.class);
+        launchLoanSummary.putExtra("report", report.toString());
+        startActivity(launchLoanSummary);
+    }
+}
 }
