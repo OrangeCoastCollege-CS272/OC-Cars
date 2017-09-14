c8d14bfc9d65d59740f021ddec1d60574ad50cb3
Badajoz <sbadajoz@student.cccd.edu>
2017-09-14 16:53:04 -0700

    Grab intent from PurchaseActivity

--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/occars/LoanSummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/occars/LoanSummaryActivity.java
@@ -1,5 +1,6 @@
 package edu.orangecoastcollege.cs273.sbadajoz.occars;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
@@ -9,6 +10,10 @@ public class LoanSummaryActivity extends AppCompatActivity {private EditText mCa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_summary);
+
+        Intent intentFromPurchase = getIntent();
+        String report = intentFromPurchase.getStringExtra("report");
+        
     }
 }
 
